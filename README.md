64157ffb1f0f54bf79a28260e01986728ba931c5
Nikorda <nikorde@gmail.com>
2020-10-05 23:03:52 +0300

    feat: add readme

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# task1
+# Task 1
+
+## [task](https://github.com/rolling-scopes-school/nodejs-course-template/blob/master/TASKS.md#task-1-caesar-cipher-cli-tool)
+## [cross-check](https://github.com/rolling-scopes-school/nodejs-course-template/blob/master/CROSSCHECK.md#task-1-caesar-cipher-cli-tool)
+
+### Options:
+- `-a` or `--action` should equal to `encode` or `decode` (required);
+- `-s` or `--shift` should be integer (required);
+- `-i` or `--input` should be a string. If the file is not found, a message will be displayed. If the parameter is absent, the result will be displayed in the console.
+- `-o` or `--output` should be a string. If the file is not found, a message will be displayed. If the parameter is absent, the result will be displayed in the console.
+
+Execute the code on the command line.
+
+### Usage example:
+
+```bash
+node index -s 7 -a encode --input "./txt/input.txt" --output "./txt/output.txt"
+```
+
+```bash
+node index -a encode -s 7 -i "./txt/input.txt" -o "./txt/output.txt"
+```
+
+```bash
+node index -s 7 --action encode
+```
+
+```bash
+node index --shift 7 -a decode
+```
+
--- caesar-cipher-cli/index.js
+++ caesar-cipher-cli/index.js
@@ -1,82 +1,96 @@
-const { program } = require('commander');
-const { caesar } = require('./js/function.js');
-const fs = require('fs');
-const { Transform, pipeline } = require('stream')
+const { program } = require("commander");
+const { caesar } = require("./js/function.js");
+const fs = require("fs");
+const { Transform, pipeline } = require("stream");
 
 const runAction = (options) => {
+  if (isNaN(options.shift)) {
+    console.error("Shift must be integer");
+    process.exit(1);
+  }
 
-    if (isNaN(options.shift)) {
-        console.error('Shift must be integer');
+  if (!(options.action === "encode" || options.action === "decode")) {
+    console.error('Action must be "encode"/"decode"');
+    process.exit(1);
+  }
+
+  if (options.input) {
+    fs.stat(options.input, (err) => {
+      if (err) {
+        err.code === "ENOENT"
+          ? console.error(`Input file ${options.input} not found`)
+          : console.error(err.message);
         process.exit(1);
-    };
-
-    if (!(options.action === 'encode' || options.action === 'decode')) {
-        console.error('Action must be "encode"/"decode"');
-        process.exit(1)
-    }
-
-    if (options.input) {
-        fs.stat(options.input, (err) => {
-            if (err) {
-                err.code === 'ENOENT' ? console.error(`Input file ${options.input} not found`) : console.error(err.message)
-                process.exit(1)
-            }
-        })
-
+      } else {
         if (options.output) {
-            fs.stat(options.output, (err) => {
-                if (err) {
-                    err.code === 'ENOENT' ? console.error(`Output file ${options.output} not found`) : console.error(err.message)
-                    process.exit(1)
-                }
-            })
-            const transtream = new Transform({
+          fs.stat(options.output, (err) => {
+            if (err) {
+              err.code === "ENOENT"
+                ? console.error(`Output file ${options.output} not found`)
+                : console.error(err.message);
+              process.exit(1);
+            } else {
+              const transtream = new Transform({
                 transform(chunk, encoding, callback) {
-                    this.push(caesar(chunk.toString(), parseInt(options.shift), options.action));
-                }
-            });
+                  this.push(
+                    caesar(
+                      chunk.toString(),
+                      parseInt(options.shift),
+                      options.action
+                    )
+                  );
+                },
+              });
 
-            pipeline(
+              pipeline(
                 fs.createReadStream(options.input),
                 transtream,
                 fs.createWriteStream(options.output, {
-                    flags: 'a'
+                  flags: "a",
                 }),
                 (err) => {
-                    if (err) {
-                        console.error('error: ', err);
-                    } else {
-                        console.log('success');
-                    }
+                  if (err) {
+                    console.error("error: ", err);
+                  } else {
+                    console.log("success");
+                  }
                 }
-            );
-        }
-        else {
-            const readStream = fs.createReadStream(options.input);
-            readStream.on('data', data => console.log(caesar(data.toString(), parseInt(options.shift), options.action)));
-        }
-    }
-    else {
-        console.log(`Enter text for ${options.action} or Ctrl+C for exit:`);
-        process.stdin.on('readable', () => {
-            const text = process.stdin.read();
-            if (text && (text == 'Ctrl+C')) {
-                process.stdin.end();
-            } else {
-                console.log(caesar(String(text), parseInt(options.shift), options.action));
-                process.stdin.resume();
-                console.log(`Enter text for ${options.action} or Ctrl+C for exit:`);
+              );
             }
-        })
-    }
+          });
+        } else {
+          const readStream = fs.createReadStream(options.input);
+          readStream.on("data", (data) =>
+            console.log(
+              caesar(data.toString(), parseInt(options.shift), options.action)
+            )
+          );
+        }
+      }
+    });
+  } else {
+    console.log(`Enter text for ${options.action} or Ctrl+C for exit:`);
+    process.stdin.on("readable", () => {
+      const text = process.stdin.read();
+      if (text && text == "Ctrl+C") {
+        process.stdin.end();
+      } else {
+        console.log(
+          caesar(String(text), parseInt(options.shift), options.action)
+        );
+        process.stdin.resume();
+        console.log(`Enter text for ${options.action} or Ctrl+C for exit:`);
+      }
+    });
+  }
 };
 
 program
-    .storeOptionsAsProperties(false)
-    .passCommandToAction(false)
-    .option('-s, --shift <number>', 'a shift')
-    .option('-i, --input <type>', 'an input file')
-    .option('-o, --output <type>', 'an output file')
-    .option('-a, --action <action>', 'an action encode/decode')
-    .action(options => runAction(options));
+  .storeOptionsAsProperties(false)
+  .passCommandToAction(false)
+  .option("-s, --shift <number>", "a shift")
+  .option("-i, --input <type>", "an input file")
+  .option("-o, --output <type>", "an output file")
+  .option("-a, --action <action>", "an action encode/decode")
+  .action((options) => runAction(options));
 program.parse(process.argv);
